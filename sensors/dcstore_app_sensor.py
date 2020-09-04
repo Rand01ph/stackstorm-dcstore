2f406fc07e6091e972f7347b077ed849beb365cb
Rand01ph <361212419@qq.com>
2020-09-04 17:44:35 +0800

    begin to code py

--- sensors/dcstore_app_sensor.py
+++ sensors/dcstore_app_sensor.py
@@ -23,6 +23,8 @@ class DcstoreAppSensor(PollingSensor):
        self._s.mount('http://', HTTPAdapter(max_retries=3))
        self._s.mount('http://', HTTPAdapter(max_retries=3))
        self._s.get(BASE_URL, timeout=5)
+       self._last_version = None
+       self._app_json = "{}/{}/{}/app.json".format(BASE_URL, self._config['category'], self._config['pkgname'])
 
    def poll(self):
        """
@@ -35,3 +37,17 @@ class DcstoreAppSensor(PollingSensor):
        st2系统停止时会执行
        """
        pass
+
+   def add_trigger(self, trigger):
+       pass
+
+   def update_trigger(self, trigger):
+       pass
+
+   def remove_trigger(self, trigger):
+       pass
+
+   def _get_last_version(self):
+       if not self._last_version and hasattr(self._sensor_service, 'get_value'):
+           self._last_version = self._sensor_service.get_value(name='last_version')
+        return self._last_version
